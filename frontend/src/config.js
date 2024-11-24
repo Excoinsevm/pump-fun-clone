913aefd3bea185f853cb9ef54ef23bc729240634
qiwihui <qwh005007@gmail.com>
2024-11-24 20:46:26 +0800

    fix: use new key

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const CONFIG = {
   CONTRACT_ADDRESS: "0x670dd4081457891775d67472df76a8e6bec1356a",
-  RPC_URL: "https://eth-sepolia.public.blastapi.io",
+  RPC_URL: "https://sepolia.infura.io/v3/748d41449342415a8097a097a9bf6387",
 };
 
 export default CONFIG;
