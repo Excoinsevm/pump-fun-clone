42bc71abee8ac5d6feb082cb5eddd27b0498ca67
qiwihui <qwh005007@gmail.com>
2024-11-24 20:37:34 +0800

    fix: fix rpc

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const CONFIG = {
   CONTRACT_ADDRESS: "0x670dd4081457891775d67472df76a8e6bec1356a",
-  RPC_URL: "https://1rpc.io/sepolia",
+  RPC_URL: "https://eth-sepolia.public.blastapi.io",
 };
 
 export default CONFIG;
