ffa9925b3882494af787b7edb0e96c4aa00d1eec
qiwihui <qwh005007@gmail.com>
2024-11-26 00:36:01 +0800

    fix: new papi

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
   CONTRACT_ADDRESS: "0x670dd4081457891775d67472df76a8e6bec1356a",
   RPC_URL: "https://sepolia.infura.io/v3/748d41449342415a8097a097a9bf6387",
-  API_URL: "http://localhost:8000",
+  API_URL: "http://pumpapi.qiwihui.com",
 };
 
 export default CONFIG;
