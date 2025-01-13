aab93d499a68335cce69c9e0bd0048376ada1b1d
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2025-01-13 03:18:17 +0000

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
 const CONFIG = {
   CONTRACT_ADDRESS: "0x670dd4081457891775d67472df76a8e6bec1356a",
-  RPC_URL: "https://sepolia.infura.io/v3/748d41449342415a8097a097a9bf6387",
+  RPC_URL: "https://connect.bit-rock.io/",
   API_URL: "https://pumpapi.qiwihui.com",
-  EXPLORER_URL: "https://sepolia.etherscan.io/",
+  EXPLORER_URL: "https://explorer.bit-rock.io/",
 };
 
 export default CONFIG;
