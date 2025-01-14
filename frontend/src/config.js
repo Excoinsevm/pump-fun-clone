d6b23895f01f87159f0a6dff4a8018c98ee2220b
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2025-01-14 13:09:18 +0000

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const CONFIG = {
-  CONTRACT_ADDRESS: "0x670dd4081457891775d67472df76a8e6bec1356a",
+  CONTRACT_ADDRESS: "0x5b133Ff50e85e0c88B191e11fD3E91F9D8BFD5bA",
   RPC_URL: "https://connect.bit-rock.io/",
   API_URL: "https://pumpapi.qiwihui.com",
   EXPLORER_URL: "https://explorer.bit-rock.io/",
